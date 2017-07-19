3d5b6900e3d9467bec726d77e556ef4877330ffc
eudes <jadieleudes@gmail.com>
2017-07-19 00:29:18 -0300

    add telas

---
+++ app/src/main/java/com/ufrpe/feelingsbox/feelingsbox/controle/gui/TelaLogin.java
@@ -0,0 +1,15 @@
+package com.ufrpe.feelingsbox.feelingsbox.controle.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ufrpe.feelingsbox.feelingsbox.R;
+
+public class TelaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_login);
+    }
+}
