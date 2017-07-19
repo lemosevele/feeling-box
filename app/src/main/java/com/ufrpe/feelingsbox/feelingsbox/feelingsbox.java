0e7fdb039ba44cb3c8bc72aa8945475e3562d6c7
evele <lemosevele@gmail.com>
2017-07-18 22:32:26 -0300

    inicial commit

---
+++ app/src/main/java/com/ufrpe/feelingsbox/feelingsbox/feelingsbox.java
@@ -0,0 +1,13 @@
+package com.ufrpe.feelingsbox.feelingsbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class feelingsbox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feelingsbox);
+    }
+}
