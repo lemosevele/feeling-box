155053e5be114c90623c35ee27541fb2496c5f49
Edson Fagner <pg2006pe@hotmail.com>
2017-07-25 18:52:04 -0300

    Melhoria no polimorfismo da GuiUtil Adicionadas as strings do GuiUtil no R.Strings.xml

--- app/src/main/java/com/ufrpe/feelingsbox/infra/GuiUtil.java
+++ app/src/main/java/com/ufrpe/feelingsbox/infra/GuiUtil.java
@@ -4,38 +4,36 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.ufrpe.feelingsbox.R;
+
 //Classe retorna elementos de texto na Tela
 public class GuiUtil {
-    public static void erroToast(Context context, Exception e){
-        Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
-    }
 
-    public static void erroToast(Context context, String text){
+    public static void myToast(Context context, String text){
         Toast.makeText(context, text, Toast.LENGTH_LONG).show();
     }
-
-    public static void erroAlertDialog(Context context, Exception e, String titulo){
-        AlertDialog.Builder dialogo = new AlertDialog.Builder(context);
-        dialogo.setTitle(titulo);
-        dialogo.setMessage(e.getMessage());
-        dialogo.setNeutralButton("Ok", null);
-        dialogo.show();
-
+    public static void myToast(Context context, Exception e){
+        GuiUtil.myToast(context, e.getMessage());
     }
-    public static void erroAlertDialog(Context context, String text, String titulo){
+
+     public static void myAlertDialog(Context context, String text, String titulo){
         AlertDialog.Builder dialogo = new AlertDialog.Builder(context);
         dialogo.setTitle(titulo);
         dialogo.setMessage(text);
-        dialogo.setNeutralButton("Ok", null);
+        dialogo.setNeutralButton(R.string.dialogo_botao_ok, null);
         dialogo.show();
 
     }
-    public static void erroAlertDialog(Context context, Exception e){
-        GuiUtil.erroAlertDialog(context, e, "Erro");
+    public static void myAlertDialog(Context context, Exception e, String titulo){
+        GuiUtil.myAlertDialog(context, e.getMessage(), titulo);
 
     }
-    public static void erroAlertDialog(Context context, String text){
-        GuiUtil.erroAlertDialog(context, text, "Erro");
+    public static void myAlertDialog(Context context, Exception e){
+        GuiUtil.myAlertDialog(context, e.getMessage(), context.getString(R.string.dialogo_titulo_erro));
+
+    }
+    public static void myAlertDialog(Context context, String text){
+        GuiUtil.myAlertDialog(context, text, context.getString(R.string.dialogo_titulo_erro));
 
     }
 }
