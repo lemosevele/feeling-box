7b803a09c799cf787c1f439c264eab00e68a6970
Eliana Maria <elianamsfcontatos@gmail.com>
2017-07-26 23:30:34 -0300

    Alterando as mensagens de erro e sucesso.

--- app/src/main/java/com/ufrpe/feelingsbox/redesocial/gui/ActEditarPerfil.java
+++ app/src/main/java/com/ufrpe/feelingsbox/redesocial/gui/ActEditarPerfil.java
@@ -1,8 +1,6 @@
 package com.ufrpe.feelingsbox.usuario.gui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
