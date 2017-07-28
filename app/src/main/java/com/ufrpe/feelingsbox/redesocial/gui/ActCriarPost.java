f2cc6d4f6f1330240ee0bbcf633763bf28ea9cec
Edson Fagner <pg2006pe@hotmail.com>
2017-07-27 23:14:17 -0300

    Adicionado Tela de Criação de Post. Aumentado o espaço entre os EditText da Tela de Cadastro

---
+++ app/src/main/java/com/ufrpe/feelingsbox/redesocial/gui/ActCriarPost.java
@@ -0,0 +1,59 @@
+package com.ufrpe.feelingsbox.redesocial.gui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ufrpe.feelingsbox.R;
+import com.ufrpe.feelingsbox.infra.GuiUtil;
+
+import java.util.zip.Inflater;
+
+public class ActCriarPost extends AppCompatActivity {
+    private TextView edtTags, edtTexto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_criar_post);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        edtTags = (TextView) findViewById(R.id.edtTags);
+        edtTexto = (TextView) findViewById(R.id.edtTexto);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_act_criar_post, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_cancelar:
+                finish();
+
+                break;
+            case R.id.action_postar:
+                GuiUtil.myToast(this, "Postado com sucesso.");
+                finish();
+
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/ufrpe/feelingsbox/redesocial/gui/ActHome.java
+++ app/src/main/java/com/ufrpe/feelingsbox/redesocial/gui/ActHome.java
@@ -46,6 +46,8 @@ public class ActHome extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), ActCriarPost.class);
+                startActivity(intent);
             }
         });
     }
