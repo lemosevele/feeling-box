f61de0f2b701b9c9910eb9dcd2806833ea2f11ec
Everton <everton.nasc43@gmail.com>
2017-08-14 17:02:36 -0300

    PerfilLogado em Sessao

--- app/src/main/java/com/ufrpe/feelingsbox/redesocial/dominio/Sessao.java
+++ app/src/main/java/com/ufrpe/feelingsbox/redesocial/dominio/Sessao.java
@@ -1,5 +1,6 @@
 package com.ufrpe.feelingsbox.redesocial.dominio;
 
+import com.ufrpe.feelingsbox.usuario.dominio.PerfilPublico;
 import com.ufrpe.feelingsbox.usuario.dominio.Pessoa;
 import com.ufrpe.feelingsbox.usuario.dominio.Usuario;
 
@@ -7,6 +8,7 @@ public class Sessao {
     private static Sessao instancia = new Sessao();
     private Pessoa pessoaLogada = null;
     private Usuario usuarioLogado = null;
+    private PerfilPublico perfillogado = null;
 
     public static Sessao getInstancia(){
 
@@ -27,8 +29,17 @@ public class Sessao {
         this.usuarioLogado = novoUsuario;
     }
 
+    public PerfilPublico getPerfillogado() {
+        return perfillogado;
+    }
+
+    public void setPerfillogado(PerfilPublico perfillogado) {
+        this.perfillogado = perfillogado;
+    }
+
     public void invalidarSessao() {
         pessoaLogada = null;
         usuarioLogado = null;
+        perfillogado = null;
     }
 }
