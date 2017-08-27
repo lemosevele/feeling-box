0ae7d076abe6343956156db3fbf128744b589d73
Edson Fagner <pg2006pe@hotmail.com>
2017-08-27 00:38:06 -0300

    Documentação do pacote (infra.provider)

--- app/src/main/java/com/ufrpe/feelingsbox/infra/provider/SearchableProvider.java
+++ app/src/main/java/com/ufrpe/feelingsbox/infra/provider/SearchableProvider.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.SearchRecentSuggestionsProvider;
 import android.provider.SearchRecentSuggestions;
 
+/**
+ * Classe com as constantes para configuração do @see {@link SearchRecentSuggestionsProvider}.
+ */
+
 public class SearchableProvider extends SearchRecentSuggestionsProvider {
     public static final String AUTHORITY = "com.ufrpe.feelingsbox.infra.provider.SearchableProvider";
     public static final int MODE = DATABASE_MODE_QUERIES;
@@ -12,12 +16,24 @@ public class SearchableProvider extends SearchRecentSuggestionsProvider {
         setupSuggestions(AUTHORITY, MODE);
     }
 
+    /**
+     * Método que registra a Tag no banco de dados do SearchRecentSuggestionsProvider.
+     * @param context - Contexto da aplicação.
+     * @param tag - Tag pesquisada pelo @see {@link com.ufrpe.feelingsbox.usuario.dominio.Usuario}.
+     */
+
     public static void salvarSugestao(Context context, String tag){
         SearchRecentSuggestions searchRecentSuggestions = new SearchRecentSuggestions(context,
                 SearchableProvider.AUTHORITY,
                 SearchableProvider.MODE);
         searchRecentSuggestions.saveRecentQuery(tag, null);
     }
+
+    /**
+     * Método que apaga todos os registros do banco de dados do SearchRecentSuggestionsProvider.
+     * @param context - Contexto da aplicação.
+     */
+
     public static void limparHistorico(Context context){
         SearchRecentSuggestions searchRecentSuggestions = new SearchRecentSuggestions(context,
                 SearchableProvider.AUTHORITY,
